6fde4bd3acc6b8d3c92cbb8a27b55d2a9d7c873d
Hinbin <nicholashoulton@gmail.com>
2019-07-13 13:23:01 +0100

    Tweaked home page and tests

--- spec/features/student_logs_into_the_site_spec.rb
+++ spec/features/student_logs_into_the_site_spec.rb
@@ -3,13 +3,16 @@ require 'support/session_helpers'
 RSpec.describe 'Student logs into the site', type: :feature, js: true do
   let(:user_login) { create(:user) }
   let(:different_login) { build(:user) }
+  before do
+    create_list(:badge, 10)
+  end
 
   context 'when logging in via Google oAuth2' do
     it 'lets a user log in to via Google oAuth2' do
       user_login
       stub_omniauth(user_login)
       visit(root_path)
-      click_link('Log in with Google')
+      click_button('Login')
       expect(page).to have_content(user_login.family_name).and have_content(user_login.given_name)
     end
 
@@ -23,7 +26,7 @@ RSpec.describe 'Student logs into the site', type: :feature, js: true do
       user_login
       stub_omniauth(different_login)
       visit(root_path)
-      click_link('Log in with Google')
+      click_button('Login')
       expect(page).to have_content('account has not been found')
     end
   end
